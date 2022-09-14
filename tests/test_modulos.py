40ded48f421bc526ecceff668df5bf292aef3da3
Alexandre Chaminas <alexandre.chaminas@keyprod.com>
2022-09-14 18:34:32 +0200

    tests fixes

--- tests/test_modulos.py
+++ tests/test_modulos.py
@@ -11,7 +11,7 @@ from modulos.modulos import euclidean_modulo, rounded_modulo, floored_modulo, ce
 
 ##### Setups #####
 
-values_test = [
+values_test_positive = [
                 55,
                 3,
                 2,
@@ -37,8 +37,13 @@ values_test = [
 
 if platform.system() != 'Windows':
     # Float128 is not available in windows, but we still want to test it to verify precision of operation within Linux
-    values_test.append(np.float128(14656199198.348697452122))
-    values_test.append(np.float128(64792397544.778624631336))
+    values_test_positive.append(np.float128(14656199198.348697452122))
+    values_test_positive.append(np.float128(64792397544.778624631336))
+
+values_test = []
+for value in values_test_positive:
+    values_test.append(value)
+    values_test.append(-value)
 
 test_combinations = []
 def construct_test_combinations():
@@ -57,6 +62,13 @@ def modulo_equations(dividend: Real, divisor: Real, remainder: Real):
 
     return ((dividend == divisor * quotient + remainder) and (abs(remainder) < abs(divisor)))
 
+def are_same_sign(a, b):
+    if (a == 0) or (b == 0):
+        return True
+    elif a > 0:
+        return b > 0
+    else:
+        return b < 0
 
 #### Euclidean modulo tests #####
 
@@ -135,7 +147,7 @@ def floored_modulo_combination_test(dividend, divisor):
         assert modulo_equations(dividend, divisor, remainder)
 
         # Assert remainder and divisor are same sign
-        assert(math.copysign(1, remainder) == math.copysign(1, divisor))
+        assert(are_same_sign(remainder, divisor))
 
         # Assert same output type to one of two inputs type
         assert((type(dividend) is type(remainder)) or (type(divisor is type(remainder))))
@@ -163,11 +175,11 @@ def ceiled_modulo_combination_test(dividend, divisor):
         # Assert basic modulos equations
         assert modulo_equations(dividend, divisor, remainder)
 
-        # Assert remainder and dividend are opposite sign (except 0 which it is both positive and negative)
+        # Assert remainder and divisor are opposite sign (except 0 which it is both positive and negative)
         if remainder > 0:
-            assert(dividend <= 0)
-        else:
-            assert(dividend >= 0)
+            assert(divisor <= 0)
+        elif remainder < 0:
+            assert(divisor >= 0)
 
         # Assert same output type to one of two inputs type
         assert((type(dividend) is type(remainder)) or (type(divisor is type(remainder))))
@@ -196,7 +208,7 @@ def truncated_modulo_combination_test(dividend, divisor):
         assert modulo_equations(dividend, divisor, remainder)
 
         # Assert remainder and dividend are same sign
-        assert(math.copysign(1, remainder) == math.copysign(1, dividend))
+        assert(are_same_sign(remainder, dividend))
 
         # Assert same output type to one of two inputs type
         assert((type(dividend) is type(remainder)) or (type(divisor is type(remainder))))
